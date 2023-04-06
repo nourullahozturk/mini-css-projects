a7c0ddbbf55c8bc3fe15d7bcc44c90e3c78827f8
nourullahozturk <nourullahozturk@gmail.com>
2023-04-06 12:35:12 +0300

    hambuger-menu component added.

---
+++ hamburger-menu/app.js
@@ -0,0 +1,6 @@
+const hamMenu = document.querySelector(".hamburger-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
